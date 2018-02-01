4696d85be8416e4a5a14b64cc20aa31e3558d5f6
Debierne Nicolas* <nicolasdebierne@outlook.com>
2018-02-01 15:17:46 +0100

    Dependencies : Bootstrap 4.0.0, Axios Start the Table Component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-row><img src="./assets/logo.png"></b-row>
+    <Table :msg="'Welcome to the DataTable'"></Table>
+
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue';
+import Table from './components/MyComponent/Table';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Table
   }
 }
 </script>
 
 <style>
+  @import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/MyComponent/Table.vue
@@ -0,0 +1,24 @@
+<template>
+    <b-row id="mainTable">
+        <table>
+            <thead></thead>
+            <tbody></tbody>
+            <tfoot></tfoot>
+        </table>
+    </b-row>
+</template>
+
+<script>
+    export default {
+        name: "Table",
+        props: {
+            msg: String
+        },
+        data() {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
