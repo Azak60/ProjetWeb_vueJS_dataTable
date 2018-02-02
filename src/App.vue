b2ac00958add2243ee757eb603bc23bf83eccbe1
Debierne Nicolas* <nicolasdebierne@outlook.com>
2018-02-02 10:03:57 +0100

    Dependencies : Bootstrap 4.0.0, Axios Start the Table Component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,61 @@
 <template>
-  <div id="app">
-    <b-row><img src="./assets/logo.png"></b-row>
-    <Table :msg="'Welcome to the DataTable'"></Table>
+    <div id="app">
+        <b-row><img src="./assets/logo.png"></b-row>
+        <Table :interventions=this.interventions :msg="'Welcome to the DataTable'"></Table>
 
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-  </div>
+        <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue';
-import Table from './components/MyComponent/Table';
+    // import HelloWorld from './components/HelloWorld.vue';
+    import Table from './components/MyComponent/Table';
 
-export default {
-  name: 'app',
-  components: {
-    Table
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            Table
+        },
+        data() {
+            return {
+                id: '',
+                title: '',
+                resume: '',
+                affectedTo: '',
+                client: '',
+                state: '',
+                interventions: [
+                    {
+                        id: '1',
+                        title: 'Correction de bug',
+                        resume: 'Correction d\'un bug impactant les serveurs back-end.',
+                        affectedTo: 'Nicolas',
+                        client: 'CGI',
+                        state: false
+                    },
+                    {
+                        id: '2',
+                        title: 'Réparation de PC',
+                        resume: 'Répération de la carte graphique défaillante',
+                        affectedTo: 'Cyril',
+                        client: 'CGI',
+                        state: false
+                    }
+                ]
+            }
+        }
+    }
 </script>
 
 <style>
-  @import '../node_modules/bootstrap/dist/css/bootstrap.css';
+    @import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/MyComponent/Table.vue
+++ src/components/MyComponent/Table.vue
@@ -1,8 +1,26 @@
 <template>
-    <b-row id="mainTable">
+    <b-row :name="mainTable" id="mainTable">
         <table>
-            <thead></thead>
-            <tbody></tbody>
+            <thead>
+            <tr>
+                <b-col-sm><th>Id</th></b-col-sm>
+                <b-col-sm><th>Title</th></b-col-sm>
+                <b-col-sm><th>Resume</th></b-col-sm>
+                <b-col-sm><th>Affected To</th></b-col-sm>
+                <b-col-sm>Client</b-col-sm>
+                <b-col-sm>State</b-col-sm>
+            </tr>
+            </thead>
+
+            <tbody>
+            <tr v-for="intervention in interventions" :key="intervention.id">
+                <td>{{intervention.id}}</td>
+                <td>{{intervention.title}}</td>
+                <td>{{intervention.resume}}</td>
+                <td>{{intervention.affectedTo}}</td>
+                <td>{{intervention.state}}</td>
+            </tr>
+            </tbody>
             <tfoot></tfoot>
         </table>
     </b-row>
@@ -12,9 +30,11 @@
     export default {
         name: "Table",
         props: {
-            msg: String
+            msg: String,
+            interventions: Array()
         },
         data() {
+            return {}
         }
     }
 </script>
